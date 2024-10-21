1d60b2c0fc63577b325b809751332d1b9426b91c
GravyLP <gravaante@outlook.com>
2024-10-21 04:59:33 +0200

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,3 +5,41 @@ burger.addEventListener('click', function(e){
 	this.classList.toggle('is-open');
 	nav.classList.toggle('is-open');
 });
+
+
+
+const observer = new IntersectionObserver((entries) =>{
+    entries.forEach((entry) =>{
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('fadeanim');
+        }
+    });
+});
+
+const hidenElements = document.querySelectorAll('.fadein');
+hidenElements.forEach((el) => observer.observe(el))
+
+const observer2 = new IntersectionObserver((entries) =>{
+    entries.forEach((entry) =>{
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('scaleanim');
+        }
+    });
+});
+
+const hidenElements2 = document.querySelectorAll('.scale');
+hidenElements2.forEach((el) => observer2.observe(el))
+
+const observer3 = new IntersectionObserver((entries) =>{
+    entries.forEach((entry) =>{
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('slideanim');
+        }
+    });
+});
+
+const hidenElements3 = document.querySelectorAll('.slidein');
+hidenElements3.forEach((el) => observer3.observe(el))
